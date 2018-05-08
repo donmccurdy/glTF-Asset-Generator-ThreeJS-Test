17ef8d859f06a664df38eed9136f020aca386a2e
Don McCurdy <don.r.mccurdy@gmail.com>
2018-05-07 21:52:44 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,7 @@
+const THREE = window.THREE = require('three');
+require('three/examples/js/loaders/GLTFLoader');
+require('three/examples/js/loaders/RGBELoader');
+require('three/examples/js/loaders/HDRCubeTextureLoader');
+require('three/examples/js/pmrem/PMREMGenerator');
+require('three/examples/js/pmrem/PMREMCubeUVPacker');
+module.exports = THREE;
